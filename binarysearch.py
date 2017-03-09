3850e87e534291647d48844090d175ddf9e9cbab
andela-omugeri <olive.nyotu@andela.com>
2017-03-09 18:08:30 +0300

    trial code to solve binary search

---
+++ binarysearch.py
@@ -0,0 +1,26 @@
+class list:
+	mydict = {}
+	a = list.length()
+	b = string.step()
+	x = index
+	length = number(len(array))
+
+
+class binarysearch(list):
+	def __init__ (self,a='length of list',b='step'):
+		self.a = length
+		self.b = count(word)
+			
+
+	def search(self,n):
+		count = result.count(word)
+		worddict[word] = word
+		x = 0
+		if x = x+1:
+			return count
+
+	
+
+					
+
+		 	
